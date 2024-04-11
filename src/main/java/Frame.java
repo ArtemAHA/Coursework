d19bf7687223bd42b7b927e519ad2fb79b66ac10
ShadowCatP <282276@student.pwr.edu.pl>
2024-04-11 12:03:38 +0200

    arranged items

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -3,58 +3,53 @@ import java.awt.*;
 
 public class Frame extends JFrame{
         Frame(){
-        super("CourseWork");
-        //Встановлюємо розмір вікна програми
-        final int width = 650;
-        final int height = 350;
-        setSize(width, height);
-        //Визначаємо розмір екрану коористувача
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        //Розміщуємо вікно програми по центру екрану
-        int x = (screenSize.width - width) / 2;
-        int y = (screenSize.height - height) / 2;
-        setLocation(x, y);
+                super("CourseWork");
+                //Window size
+                final int width = 1000;
+                final int height = 750;
 
-        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //---------------------Labels and TextFields---------------------
+                JLabel name = new JLabel("Enter name:");
+                name.setBounds(30, 40, 200, 20);
+                JTextField nameField = new JTextField();
+                nameField.setBounds(165, 40, 150, 20);
 
-        Container conteiner = super.getContentPane();
-        conteiner.setLayout(null);
+                JLabel lastName = new JLabel("Enter last name:");
+                lastName.setBounds(30, 70, 200, 20);
+                JTextField lastNameField = new JTextField();
+                lastNameField.setBounds(165, 70, 150,20);
 
-        JLabel name = new JLabel("Enter name:");
-        name.setBounds(30, 40, 200, 20);
-        JTextField name_field = new JTextField();
-        name_field.setBounds(165, 40, 150, 20);
+                JLabel discepline = new JLabel("Enter discepline:");
+                discepline.setBounds(30, 110, 200, 20);
+                //--------------------/Labels and TextFields---------------------
 
-        JLabel last_name = new JLabel("Enter last name:");
-        last_name.setBounds(30, 70, 200, 20);
-        JTextField last_name_field = new JTextField();
-        last_name_field.setBounds(165, 70, 150,20);
+                //---------------------Discipline Buttons---------------------
+                JRadioButton disceplineElectiveButton = new JRadioButton("Elective");
+                disceplineElectiveButton.setBounds(165, 110, 100, 20);
 
-        conteiner.add(name);
-        conteiner.add(name_field);
-        conteiner.add(last_name);
-        conteiner.add(last_name_field);
+                JRadioButton disceplineProgramButton = new JRadioButton("According to the program");
+                disceplineProgramButton.setBounds(265, 110, 200, 20);
 
-        JLabel disceplin = new JLabel("Зазначте тип десцепліни:");
-        disceplin.setBounds(30, 110, 200, 20);
+                ButtonGroup disceplineGroup = new ButtonGroup();
+                disceplineGroup.add(disceplineProgramButton);
+                disceplineGroup.add(disceplineElectiveButton);
+                //--------------------/Discipline Buttons---------------------
 
-        JRadioButton disceplin_viber = new JRadioButton("Вибіркова");
-        disceplin_viber.setBounds(190, 110, 90, 20);
-        JRadioButton disceplin_osnov = new JRadioButton("За програмою");
-        disceplin_osnov.setBounds(290, 110, 150, 20);
+                //---------------------Frame Setup---------------------
+                this.add(name);
+                this.add(nameField);
+                this.add(lastName);
+                this.add(lastNameField);
+                this.add(discepline);
+                this.add(disceplineElectiveButton);
+                this.add(disceplineProgramButton);
 
-        disceplin_osnov.setSelected(true);
-        ButtonGroup group = new ButtonGroup();
-        group.add(disceplin_osnov);
-        group.add(disceplin_viber);
-
-        //String[] option
-
-        conteiner.add(disceplin);
-        conteiner.add(disceplin_viber);
-        conteiner.add(disceplin_osnov);
-
-        this.setVisible(true);
-        this.setResizable(false);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.setSize(width, height);
+                this.setLocationRelativeTo(null);
+                this.setLayout(null);
+                this.setVisible(true);
+                this.setResizable(false);
+                //--------------------/Frame Setup---------------------
     }
 }
