41322a7a2f1dfc3606c3eb2121e05736fb18c0d8
Artem Hrebeniuk <artemhrebeniuk.pp@gmail.com>
2024-04-11 12:07:04 +0300

    No main

--- src/main/java/Frame.java
+++ src/main/java/Frame.java
@@ -1,62 +1,60 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Frame extends JFrame {
-    JTextField firstNameField;
-    JTextField secondNameField;
-
-    public Frame() {
+public class Frame extends JFrame{
+        Frame(){
         super("CourseWork");
-
         //Встановлюємо розмір вікна програми
         final int width = 650;
         final int height = 350;
-
+        setSize(width, height);
         //Визначаємо розмір екрану коористувача
-        //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         //Розміщуємо вікно програми по центру екрану
-        //int x = (screenSize.width - width) / 2;
-        //int y = (screenSize.height - height) / 2;
-        //setLocation(x, y);
+        int x = (screenSize.width - width) / 2;
+        int y = (screenSize.height - height) / 2;
+        setLocation(x, y);
 
-        JLabel name = new JLabel("Hello");
-        name.setBounds(30, 40, 200, 10);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container conteiner = super.getContentPane();
+        conteiner.setLayout(null);
+
+        JLabel name = new JLabel("Введіть ім'я:");
+        name.setBounds(30, 40, 200, 20);
         JTextField name_field = new JTextField();
-        name_field.setBounds(120, 37, 150, 20);
+        name_field.setBounds(165, 40, 150, 20);
 
         JLabel last_name = new JLabel("Введіть прізвище:");
+        last_name.setBounds(30, 70, 200, 20);
         JTextField last_name_field = new JTextField();
-        last_name_field.setPreferredSize(new Dimension(50, 10));
+        last_name_field.setBounds(165, 70, 150,20);
+
+        conteiner.add(name);
+        conteiner.add(name_field);
+        conteiner.add(last_name);
+        conteiner.add(last_name_field);
 
         JLabel disceplin = new JLabel("Зазначте тип десцепліни:");
-        JPanel disciplinePanel = new JPanel();
-        disciplinePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        disceplin.setBounds(30, 110, 200, 20);
 
         JRadioButton disceplin_viber = new JRadioButton("Вибіркова");
+        disceplin_viber.setBounds(190, 110, 90, 20);
         JRadioButton disceplin_osnov = new JRadioButton("За програмою");
+        disceplin_osnov.setBounds(290, 110, 150, 20);
 
         disceplin_osnov.setSelected(true);
         ButtonGroup group = new ButtonGroup();
         group.add(disceplin_osnov);
         group.add(disceplin_viber);
 
-        disciplinePanel.add(disceplin);
-        disciplinePanel.add(disceplin_viber);
-        disciplinePanel.add(disceplin_osnov);
+        //String[] option
 
-
-        this.setLayout(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setSize(width, height);
-
-        this.add(name);
-        this.add(name_field);
-        this.add(last_name);
-        this.add(last_name_field);
+        conteiner.add(disceplin);
+        conteiner.add(disceplin_viber);
+        conteiner.add(disceplin_osnov);
 
         this.setVisible(true);
+        this.setResizable(false);
     }
 }
